ae8556c963910d6328a2953731415b5e0f72df2b
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-08-20 11:10:13 +0900

    ✨ feat: 曜日を定数化
    曜日を定数化

---
+++ src/constants/weekDay.ts
@@ -0,0 +1,10 @@
+export type WeekDays = 'SU' | 'MO' | 'TU' | 'WE' | 'TH' | 'FR' | 'SA'
+export const weekDayList: Record<number, WeekDays> = {
+  0: 'SU',
+  1: 'MO',
+  2: 'TU',
+  3: 'WE',
+  4: 'TH',
+  5: 'FR',
+  6: 'SA',
+}
