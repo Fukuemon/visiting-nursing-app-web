8451aa9291939e74ee48f9ba31c2953124f039df
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-09-10 11:50:52 +0900

    ✨ feat: 繰り返しルールの定数化
    繰り返しルールの定数化

---
+++ src/constants/recallingRule.ts
@@ -0,0 +1,41 @@
+import type { RecallingRule } from '@/types/recallingRule'
+
+export const RecallingFrequency = {
+  everyWeek: 'everyWeek',
+  everyOtherWeek: 'everyOtherWeek',
+  onceAMonth: 'onceAMonth',
+} as const
+
+export type RecallingFrequency =
+  (typeof RecallingFrequency)[keyof typeof RecallingFrequency]
+
+export const RecallingFrequencyText: Record<RecallingFrequency, string> = {
+  [RecallingFrequency.everyWeek]: '週一',
+  [RecallingFrequency.everyOtherWeek]: '隔週',
+  [RecallingFrequency.onceAMonth]: '月一',
+} as const
+
+export const recallingTextFromRule = (recalling_rule: RecallingRule) => {
+  if (
+    recalling_rule.frequency === 1 &&
+    recalling_rule.interval === 1 &&
+    recalling_rule.dayOfWeek !== undefined
+  ) {
+    return RecallingFrequencyText[RecallingFrequency.everyWeek]
+  }
+  if (
+    recalling_rule.frequency === 1 &&
+    recalling_rule.interval === 2 &&
+    recalling_rule.dayOfWeek !== undefined
+  ) {
+    return RecallingFrequencyText[RecallingFrequency.everyOtherWeek]
+  }
+  if (
+    recalling_rule.frequency === 1 &&
+    recalling_rule.interval === 1 &&
+    recalling_rule.dayOfMonth !== undefined
+  ) {
+    return RecallingFrequencyText[RecallingFrequency.onceAMonth]
+  }
+  return RecallingFrequencyText[RecallingFrequency.everyWeek]
+}
