7eee0760c3d7fc67c63d00d8a929a2f49fb48fbe
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-09-10 11:48:20 +0900

    ✨ feat: スケジュールの各ページ
    スケジュールの各ページ

---
+++ src/app/calendar/schedule/[scheduleId]/page.tsx
@@ -0,0 +1,6 @@
+'use client'
+import { ScheduleDetailContainer } from '@/app/calendar/schedule/[scheduleId]/_components/ScheduleDetailContainer'
+
+export default function ScheduleDetail() {
+  return <ScheduleDetailContainer />
+}
