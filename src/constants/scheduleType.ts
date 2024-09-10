792ecd9c1ab2e3e30da2b4d80db2de72e7f1ccad
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-09-10 11:54:27 +0900

    ✨ feat: スケジュールタイプの定数化
    スケジュールタイプの定数化

---
+++ src/constants/scheduleType.ts
@@ -0,0 +1,16 @@
+export const ScheduleType = {
+  normal: 'normal',
+  normalRecalling: 'normalRecalling',
+  visit: 'visit',
+  visitRecalling: 'visitRecalling',
+} as const
+
+export type ScheduleType = (typeof ScheduleType)[keyof typeof ScheduleType]
+
+export const ScheduleTypeText: Record<ScheduleType, string> = {
+  [ScheduleType.normal]: '通常',
+  [ScheduleType.normalRecalling]: '通常繰り返し',
+  [ScheduleType.visit]: '訪問',
+  [ScheduleType.visitRecalling]: '定期訪問',
+} as const
+
