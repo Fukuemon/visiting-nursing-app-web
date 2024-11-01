79fac2eabed17d5a23d7d5cd392faf1b4d1aa3a1
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-11-01 11:44:43 +0900

    🔀 change: 予定と繰り返し予定の分離
    予定と繰り返し予定のスキーマを分離して別々で編集できるように

--- src/schema/schedule.ts
+++ src/schema/schedule.ts
@@ -1,189 +1,133 @@
-import { ScheduleCategory } from '@/constants/scheduleCatefory'
+import type { CcCategory } from '@/constants/ccCategory'
+import { ccCategory } from '@/constants/ccCategory'
+import type { ScheduleCategory } from '@/constants/scheduleCategory'
+import { scheduleCategoryConstant } from '@/constants/scheduleCategory'
 import { scheduleType } from '@/constants/scheduleType'
-import { serviceCode, type ServiceCode } from '@/constants/serviceCode'
+import type { ServiceCode } from '@/constants/serviceCode'
+import { serviceCode } from '@/constants/serviceCode'
 import type { ZodType } from 'zod'
 import { z } from 'zod'
+import type { RecallingSchedule } from './recallingSchedule'
+import { recallingScheduleSchema } from './recallingSchedule'
 
 export enum ScheduleKey {
   ScheduleId = 'scheduleId',
-  User='user',
+  RecallingSchedule = 'recallingSchedule',
+  UserId = 'userId',
+  RecallingScheduleId = 'recallingScheduleId',
+  CcCategory = 'ccCategory',
+  CcUserId = 'ccUserId',
   Title = 'title',
+  ScheduleDate = 'scheduleDate',
   StartTime = 'startTime',
   EndTime = 'endTime',
-  IsModified = 'isModified',
   ScheduleType = 'scheduleType',
+  Description = 'description',
 }
 
 export type BaseSchedule = {
   [ScheduleKey.ScheduleId]: string
-  [ScheduleKey.User]: string
+  [ScheduleKey.RecallingSchedule]?: RecallingSchedule
+  [ScheduleKey.RecallingScheduleId]?: string
+  [ScheduleKey.UserId]: string
+  [ScheduleKey.CcCategory]?: CcCategory
+  [ScheduleKey.CcUserId]?: string
   [ScheduleKey.Title]: string
-  [ScheduleKey.StartTime]: Date
-  [ScheduleKey.EndTime]: Date
-  [ScheduleKey.IsModified]: boolean
+  [ScheduleKey.ScheduleDate]: Date
+  [ScheduleKey.StartTime]: string
+  [ScheduleKey.EndTime]: string
+  [ScheduleKey.Description]: string
 }
 
-export const baseScheduleSchema = z.object({
-  [ScheduleKey.ScheduleId]: z.string().uuid(),
-  [ScheduleKey.User]: z.string().uuid(),
+const baseScheduleSchema = z.object({
+  [ScheduleKey.ScheduleId]: z.string().ulid(),
+  [ScheduleKey.RecallingSchedule]: z
+    .lazy(() => recallingScheduleSchema)
+    .optional(),
+  [ScheduleKey.RecallingScheduleId]: z.string().ulid().optional(),
+  [ScheduleKey.UserId]: z.string().ulid(),
+  [ScheduleKey.CcCategory]: z.nativeEnum(ccCategory).optional(),
+  [ScheduleKey.CcUserId]: z.string().ulid().optional(),
   [ScheduleKey.Title]: z.string(),
-  [ScheduleKey.StartTime]: z.date(),
-  [ScheduleKey.EndTime]: z.date(),
-  [ScheduleKey.IsModified]: z.boolean(),
-})
+  [ScheduleKey.ScheduleDate]: z.date(),
+  [ScheduleKey.StartTime]: z.string(),
+  [ScheduleKey.EndTime]: z.string(),
+  [ScheduleKey.Description]: z.string(),
+}) satisfies ZodType<BaseSchedule>
 
-export const enum VisitScheduleKey {
-    Patient= 'patient',
+export enum VisitScheduleKey {
+  PatientId = 'patientId',
   ScheduleCategory = 'scheduleCategory',
   Destination = 'destination',
   ServiceCode = 'serviceCode',
+  ServiceTime = 'serviceTime',
   IsCanceled = 'isCanceled',
 }
 
 export type VisitSchedule = BaseSchedule & {
   [ScheduleKey.ScheduleType]: scheduleType.visit
-  [VisitScheduleKey.Patient]: string
-  [VisitScheduleKey.ScheduleCategory]: ScheduleCategory
+  [VisitScheduleKey.PatientId]: string
+  [VisitScheduleKey.ScheduleCategory]?: ScheduleCategory[]
   [VisitScheduleKey.ServiceCode]: ServiceCode
   [VisitScheduleKey.Destination]: string
   [VisitScheduleKey.IsCanceled]: boolean
+  [VisitScheduleKey.ServiceTime]: number
 }
 
-export const VisitScheduleSchema: ZodType<VisitSchedule> = baseScheduleSchema.and(
-  z.object({
-    [ScheduleKey.ScheduleType]: z.literal(scheduleType.visit),
-    [VisitScheduleKey.Patient]: z.string().uuid(),
-    [VisitScheduleKey.ScheduleCategory]: z.nativeEnum(ScheduleCategory),
-    [VisitScheduleKey.ServiceCode]: z.nativeEnum(serviceCode),
-    [VisitScheduleKey.Destination]: z.string(),
-    [VisitScheduleKey.IsCanceled]: z.boolean(),
-  }),
-)
-
-export const enum NormalScheduleKey {
-  Description = 'description',
-}
+export const visitScheduleSchema = baseScheduleSchema.extend({
+  [ScheduleKey.ScheduleType]: z.literal(scheduleType.visit),
+  [VisitScheduleKey.PatientId]: z.string().ulid(),
+  [VisitScheduleKey.ScheduleCategory]: z
+    .array(z.nativeEnum(scheduleCategoryConstant))
+    .optional(),
+  [VisitScheduleKey.ServiceCode]: z.nativeEnum(serviceCode),
+  [VisitScheduleKey.Destination]: z.string(),
+  [VisitScheduleKey.IsCanceled]: z.boolean(),
+  [VisitScheduleKey.ServiceTime]: z.number().min(0),
+}) satisfies ZodType<VisitSchedule>
 
 export type NormalSchedule = BaseSchedule & {
   [ScheduleKey.ScheduleType]: scheduleType.normal
-  [NormalScheduleKey.Description]: string
 }
 
-export const normalScheduleSchema: ZodType<NormalSchedule> =
-  baseScheduleSchema.and(
-    z.object({
-      [ScheduleKey.ScheduleType]: z.literal(scheduleType.normal),
-      [NormalScheduleKey.Description]: z.string(),
-    }),
-  )
+export const normalScheduleSchema = baseScheduleSchema.extend({
+  [ScheduleKey.ScheduleType]: z.literal(scheduleType.normal),
+}) satisfies ZodType<NormalSchedule>
 
-export enum RecallingFrequency {
-  Weekly = 'Weekly',
-  Monthly = 'Monthly',
-}
-export const enum RecallingRuleKey {
-  Frequency = 'frequency',
-  DayOfWeek = 'dayOfWeek',
-  WeekOfMonth = 'weekOfMonth',
-  DayOfMonth = 'dayOfMonth',
-}
+export type Schedule = NormalSchedule | VisitSchedule
 
-export type RecallingRuleWeekly = {
-  [RecallingRuleKey.Frequency]: RecallingFrequency.Weekly
-  [RecallingRuleKey.DayOfWeek]: number
-}
-
-export type RecallingRuleWeekOfMonth = {
-  [RecallingRuleKey.Frequency]: RecallingFrequency.Monthly
-  [RecallingRuleKey.WeekOfMonth]: number
-}
-
-export type RecallingRuleMonthly = {
-  [RecallingRuleKey.Frequency]: RecallingFrequency.Monthly
-  [RecallingRuleKey.DayOfMonth]: number
-}
-
-export type RecallingRule =
-  | RecallingRuleWeekly
-  | RecallingRuleWeekOfMonth
-  | RecallingRuleMonthly
-
-export const RecallingRuleSchema: ZodType<RecallingRule> = z.discriminatedUnion(
-  'frequency',
-  [
-    z.object({
-      [RecallingRuleKey.Frequency]: z.literal(RecallingFrequency.Weekly),
-      [RecallingRuleKey.DayOfWeek]: z.number().min(0).max(6),
-    }),
-    z.object({
-      [RecallingRuleKey.Frequency]: z.literal(RecallingFrequency.Monthly),
-      [RecallingRuleKey.WeekOfMonth]: z.number().min(1).max(5),
-    }),
-    z.object({
-      [RecallingRuleKey.Frequency]: z.literal(RecallingFrequency.Monthly),
-      [RecallingRuleKey.DayOfMonth]: z.number().min(1).max(31),
-    }),
-  ],
-)
-
-export const enum RecallingScheduleKey {
-  RecallingRule = 'recallingRule',
-  StartTime = 'startTime',
-  EndTime = 'endTime',
-}
-
-export type VisitRecallingSchedule = VisitSchedule & {
-  [RecallingScheduleKey.RecallingRule]: RecallingRule
-}
-
-export const visitRecallingScheduleSchema: ZodType<VisitRecallingSchedule> =
-  VisitScheduleSchema.and(
-    z.object({
-      [RecallingScheduleKey.RecallingRule]: RecallingRuleSchema,
-    }),
-  )
-
-export type NormalRecallingSchedule = NormalSchedule & {
-  [RecallingScheduleKey.RecallingRule]: RecallingRule
-}
-
-export const normalRecallingScheduleSchema: ZodType<NormalRecallingSchedule> =
-  normalScheduleSchema.and(
-    z.object({
-      [RecallingScheduleKey.RecallingRule]: RecallingRuleSchema,
-    }),
-  )
-
-export type Schedule = BaseSchedule &
-  (
-    | NormalSchedule
-    | VisitSchedule
-    | NormalRecallingSchedule
-    | VisitRecallingSchedule
-  )
-
-export type ScheduleEdit = Schedule
+export type VisitScheduleEdit = Omit<
+  VisitSchedule,
+  ScheduleKey.RecallingSchedule
+>
+export type NormalScheduleEdit = Omit<
+  NormalSchedule,
+  ScheduleKey.RecallingSchedule
+>
+export type ScheduleEdit = VisitScheduleEdit | NormalScheduleEdit
 
 export type ScheduleCreate = Omit<Schedule, ScheduleKey.ScheduleId>
 
-export const scheduleSchema:ZodType<Schedule> = baseScheduleSchema.and(
-    z.union([
-        VisitScheduleSchema,
-        visitRecallingScheduleSchema,
-        normalScheduleSchema,
-        normalRecallingScheduleSchema,
-    ])
-)
+export const scheduleSchema = baseScheduleSchema.and(
+  z.union([visitScheduleSchema, normalScheduleSchema]),
+) satisfies ZodType<Schedule>
 
-export const scheduleEditSchema: ZodType<ScheduleEdit> = scheduleSchema
+export const visitScheduleEditSchema = visitScheduleSchema.omit({
+  [ScheduleKey.RecallingSchedule]: true,
 
-export const scheduleCreateSchema: ZodType<ScheduleCreate> = baseScheduleSchema
-  .omit({ [ScheduleKey.ScheduleId]: true })
-  .and(
-    z.union([
-      VisitScheduleSchema,
-      visitRecallingScheduleSchema,
-      normalScheduleSchema,
-      normalRecallingScheduleSchema,
-    ]),
-  )
+}) satisfies ZodType<VisitScheduleEdit>
+
+export const normalScheduleEditSchema = normalScheduleSchema.omit({
+  [ScheduleKey.RecallingSchedule]: true,
+
+}) satisfies ZodType<NormalScheduleEdit>
+
+export const scheduleEditSchema = z.union([
+  visitScheduleEditSchema,
+  normalScheduleEditSchema,
+]) satisfies ZodType<ScheduleEdit>
+
+export const scheduleCreateSchema = z.union([
+  visitScheduleSchema.omit({ [ScheduleKey.ScheduleId]: true }),
+  normalScheduleSchema.omit({ [ScheduleKey.ScheduleId]: true }),
+]) satisfies ZodType<ScheduleCreate>
