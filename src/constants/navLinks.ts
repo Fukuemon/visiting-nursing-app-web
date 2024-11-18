fecc0a987859d48fae97ca1529acba328d45b072
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-11-18 10:51:33 +0900

    🔀 change: hrefにpathpida生成のpathを使う
    hrefにpathpida生成のpathを使う

--- src/constants/navLinks.ts
+++ src/constants/navLinks.ts
@@ -1,6 +1,8 @@
+import { pagesPath } from '@/utils/$path'
+
 export const LINKS = [
-  { href: '/calendar', label: 'カレンダー' },
+  { href: pagesPath.calendar.$url().path, label: 'カレンダー' },
   { href: '/patient', label: '患者' },
-  { href: '/staff', label: '職員情報' },
+  { href: pagesPath.users.$url().path, label: '職員情報' },
   { href: '/indicators', label: '経営数値' },
 ]
