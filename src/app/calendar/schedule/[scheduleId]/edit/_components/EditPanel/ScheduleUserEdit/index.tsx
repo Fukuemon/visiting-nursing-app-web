66a76cf5f04aa5262d8b9d4c76fd46c922d2ad62
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-11-19 09:26:47 +0900

    🔀 change: APIからデータフェッチ
    バックエンドのAPIにリクエストを送りデータフェッチ

--- src/app/calendar/schedule/[scheduleId]/edit/_components/EditPanel/ScheduleUserEdit/index.tsx
+++ src/app/calendar/schedule/[scheduleId]/edit/_components/EditPanel/ScheduleUserEdit/index.tsx
@@ -1,13 +1,14 @@
 import type { Control, FieldPath, FieldValues } from 'react-hook-form'
-import { useController } from 'react-hook-form'
+import { useController, useForm } from 'react-hook-form'
 
+import { Loading } from '@/app/_components/Loading'
+import { SearchInput } from '@/app/_components/SearchInput'
+import { SelectButton } from '@/app/_components/SelectButton'
 import Table from '@/app/_components/Table'
-import { TextInput } from '@/app/_components/TextInput'
-import { useUserList } from '@/hooks/user'
-import type { User } from '@/types/user'
-import { useState } from 'react'
+import { useTeamList } from '@/hooks/api/team'
+import { useUserList } from '@/hooks/api/user'
+import { useDebounce } from '@/hooks/useDebounce'
 import styles from './style.module.css'
-import SearchIcon from '/public/icons/search.svg'
 
 export type ScheduleUserEditProps<T extends FieldValues> = {
   control: Control<T>
@@ -18,64 +19,77 @@ export const ScheduleUserEdit = <T extends FieldValues>({
   control,
   name,
 }: ScheduleUserEditProps<T>) => {
-  const { users, isLoading, error } = useUserList()
+  const facilityId = '01J6SMYDSKKKNJCR2Y3242T7YX'
+  const { teams, error: teamsError } = useTeamList(facilityId)
+  const {
+    control: controlSearch,
+    watch: watchSearch,
+    handleSubmit,
+  } = useForm<{
+    username: string
+    team: string
+  }>({ defaultValues: { username: '', team: '' } })
+  const { users, error, mutate } = useUserList([
+    facilityId,
+    watchSearch('username'),
+    '',
+    '',
+    watchSearch('team'),
+  ])
 
   const { field } = useController({
     name,
     control,
   })
 
-  const [searchText, setSearchText] = useState('')
-  const [searchTextTeam, setSearchTextTeam] = useState('')
-  const [searchUsers, setSearchUsers] = useState<User[]>(users ?? [])
-  if (isLoading || users === undefined) return <div>Loading...</div>
   if (error !== undefined && error.message !== '') {
     throw new Error(error.message)
   }
-
-  const handleSearch = () => {
-    const filteredUsers = users.filter((user) => {
-      const matchesName =
-        searchText === '' ||
-        user.name.toLowerCase().includes(searchText.toLowerCase())
-      const matchesTeam =
-        searchTextTeam === '' ||
-        user.team.toLowerCase().includes(searchTextTeam.toLowerCase())
-      return matchesName && matchesTeam
-    })
-    setSearchUsers(filteredUsers)
+  if (teamsError !== undefined && teamsError.message !== '') {
+    throw new Error(teamsError.message)
   }
-  const searchIcon = (
-    <button onClick={handleSearch}>
-      <SearchIcon />
-    </button>
+
+  useDebounce<{ search: string }>(
+    () => {
+      mutate()
+    },
+    {
+      value: { search: watchSearch('username') },
+      debounceMs: 500,
+    },
   )
   return (
     <div className={styles.userEdit}>
-      <div className={styles.searchInput}>
-        <TextInput
+      <form
+        className={styles.searchInput}
+        onSubmit={handleSubmit(() => {
+          mutate()
+        })}
+      >
+        <SearchInput
           placeholder="メンバーを検索"
-          icon={searchIcon}
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleSearch()
-            }
-          }}
+          control={controlSearch}
+          name="username"
         />
-        <TextInput
-          placeholder="チームを検索"
-          icon={searchIcon}
-          value={searchTextTeam}
-          onChange={(e) => setSearchTextTeam(e.target.value)}
-          onKeyDown={(e) => {
-            if (e.key === 'Enter') {
-              handleSearch()
-            }
-          }}
-        />
-      </div>
+        {teams === undefined ? (
+          <Loading />
+        ) : (
+          <SelectButton
+            control={controlSearch}
+            name="team"
+            options={[
+              {
+                label: 'すべてのチーム',
+                value: '',
+              },
+              ...teams.map((team) => ({
+                label: team.name,
+                value: team.name,
+              })),
+            ]}
+          />
+        )}
+      </form>
       <div className={styles.table}>
         <Table>
           <Table.Header>
@@ -85,7 +99,7 @@ export const ScheduleUserEdit = <T extends FieldValues>({
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {searchUsers.map((user) => (
+            {users?.map((user) => (
               <Table.Row key={user.id}>
                 <Table.Cell>
                   <label className={styles.option}>
@@ -96,7 +110,7 @@ export const ScheduleUserEdit = <T extends FieldValues>({
                       value={user.id}
                       checked={field.value === user.id}
                     />
-                    <span className={styles.label}>{user.name}</span>
+                    <span className={styles.label}>{user.username}</span>
                     <span className={styles.label}>{user.team}</span>
                   </label>
                 </Table.Cell>
--- src/app/calendar/schedule/[scheduleId]/edit/_components/ScheduleEditContainer/index.tsx
+++ src/app/calendar/schedule/[scheduleId]/edit/_components/ScheduleEditContainer/index.tsx
@@ -1,7 +1,8 @@
 'use client'
+import { Loading } from '@/app/_components/Loading'
 import { EditSchedule } from '@/app/calendar/schedule/[scheduleId]/edit/_components/EditSchedule'
-import { useSchedule } from '@/hooks/schedule'
-import { useUserList } from '@/hooks/user'
+import { useSchedule } from '@/hooks/api/schedule'
+import { useUserList } from '@/hooks/api/user'
 import { useParams } from 'next/navigation'
 import type { FC } from 'react'
 import styles from './style.module.css'
@@ -9,15 +10,11 @@ import styles from './style.module.css'
 export type ScheduleEditContainerProps = object
 
 export const ScheduleEditContainer: FC<ScheduleEditContainerProps> = ({}) => {
+  const facilityId = '01J6SMYDSKKKNJCR2Y3242T7YX'
   const { scheduleId } = useParams<{ scheduleId: string }>()
   const schedule = useSchedule(scheduleId)
-  const users = useUserList()
-  if (
-    schedule.isLoading ||
-    schedule.schedule === undefined ||
-    users.isLoading ||
-    users.users === undefined
-  )
+  const users = useUserList([facilityId, '', '', '', ''])
+  if (schedule.schedule === undefined || users.users === undefined)
     return <div>Loading...</div>
   if (
     schedule.error !== undefined &&
@@ -29,7 +26,11 @@ export const ScheduleEditContainer: FC<ScheduleEditContainerProps> = ({}) => {
   }
   return (
     <div className={styles.scheduleEditContainer}>
-      <EditSchedule schedule={schedule.schedule} users={users.users} />
+      {schedule.schedule === undefined || users.users === undefined ? (
+        <Loading />
+      ) : (
+        <EditSchedule schedule={schedule.schedule} users={users.users} />
+      )}
     </div>
   )
 }
