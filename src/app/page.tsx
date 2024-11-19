012476266502d2915b1e45fbbb33d4bca979a5eb
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-11-19 11:29:47 +0900

    ✨ feat: 仮のログインページ
    仮のログインページ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 'use client'
 
+import { pagesPath } from "@/utils/$path"
+import Link from "next/link"
+
 export default function Home() {
-  return <main></main>
+  const facilityId = '01J6SMYDSKKKNJCR2Y3242T7YX'
+  return (
+    <main>
+      <Link href={pagesPath._facilityId(facilityId).calendar.$url().path}>
+        ログイン
+      </Link>
+    </main>
+  )
 }
