d4ba18716197012301ca57bc0f3c5ab3ddfd035f
YoshitakaKyougoku <114034035+YoshitakaKyougoku@users.noreply.github.com>
2024-11-22 11:36:30 +0900

    💄 style: サイドナビゲーションUI修正
    UIの崩れの修正とアイコンの追加

--- src/app/[facilityId]/users/page.tsx
+++ src/app/[facilityId]/users/page.tsx
@@ -4,30 +4,14 @@ import { UsersContainer } from '@/app/[facilityId]/users/_components/UsersContai
 import { Header } from '@/app/_components/Header'
 import { IconButton } from '@/app/_components/IconButton'
 import { Menubar } from '@/app/_components/Menubar'
-import { pagesPath } from '@/utils/$path'
-import { useParams, usePathname } from 'next/navigation'
+import { usePathname } from 'next/navigation'
 import { useState } from 'react'
 import MenuIcon from '/public/icons/menu.svg'
 
 export default function Users() {
   const [isMenubarOpen, setIsMenubarOpen] = useState(false)
-  const { facilityId } = useParams<{ facilityId: string }>()
   const pathname = usePathname()
-  const LINKS = [
-    {
-      href: pagesPath._facilityId(facilityId).calendar.$url().path,
-      label: 'カレンダー',
-    },
-    { href: '/patient', label: '患者' },
-    {
-      href: pagesPath._facilityId(facilityId).users.$url().path,
-      label: '職員情報',
-    },
-    { href: '/indicators', label: '経営数値' },
-  ]
-  const menubar = (
-    <Menubar links={LINKS} currentLink={pathname} isOpen={isMenubarOpen} />
-  )
+  const menubar = <Menubar currentLink={pathname} isOpen={isMenubarOpen} />
   const leftIcon = (
     <IconButton onClick={() => setIsMenubarOpen(!isMenubarOpen)}>
       <MenuIcon height={32} width={32} />
--- src/app/_components/Menubar/index.tsx
+++ src/app/_components/Menubar/index.tsx
@@ -1,16 +1,36 @@
+import { pagesPath } from '@/utils/$path'
 import classNames from 'classnames'
 import { easeOut, motion } from 'framer-motion'
 import Link from 'next/link'
+import { useParams } from 'next/navigation'
 import type { FC } from 'react'
 import styles from './style.module.css'
+import ChartIcon from '/public/icons/bar-chart-4.svg'
+import CalendarIcon from '/public/icons/calendar.svg'
+import PatientIcon from '/public/icons/contact-round.svg'
+import UsersIcon from '/public/icons/users.svg'
 
 type MenubarProps = {
   isOpen: boolean
   currentLink: string
-  links: { href: string; label: string }[]
 }
 
-export const Menubar: FC<MenubarProps> = ({ isOpen, currentLink, links }) => {
+export const Menubar: FC<MenubarProps> = ({ isOpen, currentLink }) => {
+  const { facilityId } = useParams<{ facilityId: string }>()
+  const links = [
+    {
+      href: pagesPath._facilityId(facilityId).calendar.$url().path,
+      label: 'カレンダー',
+      icon: <CalendarIcon />,
+    },
+    { href: '/patient', label: '患者', icon: <PatientIcon /> },
+    {
+      href: pagesPath._facilityId(facilityId).users.$url().path,
+      label: '職員情報',
+      icon: <UsersIcon />,
+    },
+    { href: '/indicators', label: '経営数値', icon: <ChartIcon /> },
+  ]
   return (
     <motion.div
       className={styles.menubarContainer}
@@ -31,15 +51,16 @@ export const Menubar: FC<MenubarProps> = ({ isOpen, currentLink, links }) => {
       <div className={styles.menuContent}>
         <div className={styles.links}>
           {links.map((link) => (
-            <Link
-              key={link.href}
-              className={classNames(styles.link, {
-                [styles.currentLink]: currentLink === link.href,
+            <div
+              className={classNames(styles.linkContainer, {
+                [styles._currentLink]: currentLink === link.href,
               })}
-              href={link.href}
             >
-              {link.label}
-            </Link>
+              <Link key={link.href} className={styles.link} href={link.href}>
+                <div className={styles.linkIcon}>{link.icon}</div>
+                {link.label}
+              </Link>
+            </div>
           ))}
         </div>
       </div>
